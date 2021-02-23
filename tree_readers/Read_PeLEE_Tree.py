9f748c9b99e999038ce827aafb87e9a8b7b004ad
jingyuanshi <jingyuan.shi-2@postgrad.manchester.ac.uk>
2021-02-23 09:30:36 -0600

    This version adds numbers to the legend

--- tree_readers/Read_PeLEE_Tree.py
+++ tree_readers/Read_PeLEE_Tree.py
@@ -171,15 +171,19 @@ df_numu_EXT['POT_weight']    = 0.257
 
 frames = [df_1enp_nue, df_1enp_MC_BNB, df_1enp_CC0pi, df_1enp_CCpi0, df_1enp_NCCpi, df_1enp_NC0pi, df_1enp_NCpi0, df_1enp_CCCpi,
           df_1e0p_nue, df_1e0p_MC_BNB, df_1e0p_CC0pi, df_1e0p_CCpi0, df_1e0p_NCCpi, df_1e0p_NC0pi, df_1e0p_NCpi0, df_1e0p_CCCpi,
-          df_numu_MC_BNB, df_numu_DIRT]
+          df_numu_MC_BNB]
+frames_dirt = [df_1e0p_DIRT,df_numu_DIRT]
 frames_ext = [df_1enp_EXT, df_1e0p_EXT, df_numu_EXT]
 df_no_energy_cut_total = pd.concat(frames)
 df_no_energy_cut_ext = pd.concat(frames_ext)
+df_no_energy_cut_dirt = pd.concat(frames_dirt)
 df_no_energy_cut_total.enu_reco *= 1000#Now the reco energy is in [MeV]
 df_no_energy_cut_ext.enu_reco *= 1000
+df_no_energy_cut_dirt.enu_reco *= 1000
 
 df_no_energy_cut_total['POT_Times_weight'] = df_no_energy_cut_total['POT_weight'] * df_no_energy_cut_total['event_weight']
 df_no_energy_cut_ext['POT_Times_weight'] = df_no_energy_cut_ext['POT_weight'] * df_no_energy_cut_ext['event_weight']
+df_no_energy_cut_dirt['POT_Times_weight'] = df_no_energy_cut_dirt['POT_weight'] * df_no_energy_cut_dirt['event_weight']
 
 ################
 ##Energy range##
@@ -190,6 +194,7 @@ LOWER_LIMIT = 150
 
 df=df_no_energy_cut_total[(df_no_energy_cut_total['enu_reco']<UPPER_LIMIT) & (df_no_energy_cut_total['enu_reco']>LOWER_LIMIT)]
 df_ext=df_no_energy_cut_ext[(df_no_energy_cut_ext['enu_reco']<UPPER_LIMIT) & (df_no_energy_cut_ext['enu_reco']>LOWER_LIMIT)]
+df_dirt=df_no_energy_cut_dirt[(df_no_energy_cut_dirt['enu_reco']<UPPER_LIMIT) & (df_no_energy_cut_dirt['enu_reco']>LOWER_LIMIT)]
 #df['reco_with_weight']     = df['enu_reco'] * df['event_weight'] #Bullshit
 
 hknumuCCQE     = df[(df['IsNC']==0) & ((df['nu_pdg_final']== 14) | (df['nu_pdg_final']== -14))  & (df['nu_interaction_mode'] == 0)]#pdg == +-14 means muon and +-12 is electron
@@ -201,12 +206,21 @@ hkNCInclusive  = df[(df['IsNC']==1)]
 
 #Calculate total entries and normalize according to it
 #total_counts = hknumuCCQE.event_weight.sum() + hknumuRes.event_weight.sum() + hknumuMEC.event_weight.sum() + hknumuCCOther.event_weight.sum() + hknuEInclusive.event_weight.sum() + hkNCInclusive.event_weight.sum()+df_ext.event_weight.sum()
-total_counts = hknumuCCQE.POT_Times_weight.sum() + hknumuRes.POT_Times_weight.sum() + hknumuMEC.POT_Times_weight.sum() + hknumuCCOther.POT_Times_weight.sum() + hknuEInclusive.POT_Times_weight.sum() + hkNCInclusive.POT_Times_weight.sum()+df_ext.POT_Times_weight.sum()
-df['norm'] = 1/total_counts
-df['norm_weight'] = df['POT_Times_weight'] * df['norm']
+Normalization = False
+if Normalization is True:
+    total_counts = hknumuCCQE.POT_Times_weight.sum() + hknumuRes.POT_Times_weight.sum() + hknumuMEC.POT_Times_weight.sum() + hknumuCCOther.POT_Times_weight.sum() + hknuEInclusive.POT_Times_weight.sum() + hkNCInclusive.POT_Times_weight.sum()+df_ext.POT_Times_weight.sum()+df_dirt.POT_Times_weight.sum()
+    df['norm'] = 1/total_counts
+    df['norm_weight'] = df['POT_Times_weight'] * df['norm']
+    
+    df_ext['norm'] = 1/total_counts
+    df_ext['norm_weight'] = df_ext['POT_Times_weight'] * df_ext['norm']
 
-df_ext['norm'] = 1/total_counts
-df_ext['norm_weight'] = df_ext['POT_Times_weight'] * df_ext['norm']
+    df_dirt['norm'] = 1/total_counts
+    df_dirt['norm_weight'] = df_dirt['POT_Times_weight'] * df_dirt['norm']
+else:
+    df['norm_weight'] = df['POT_Times_weight'] *6.37/2.13#6.37 over 2.13 is the POT scaling?
+    df_ext['norm_weight'] = df_ext['POT_Times_weight'] *6.37/2.13
+    df_dirt['norm_weight'] = df_dirt['POT_Times_weight'] * 6.37/2.13
 
 hknumuCCQE_enu_reco     = df.enu_reco[(df['IsNC']==0) & ((df['nu_pdg_final']== 14) | (df['nu_pdg_final']== -14))  & (df['nu_interaction_mode'] == 0)]#pdg == +-14 means muon and +-12 is electron
 hknumuRes_enu_reco      = df.enu_reco[(df['IsNC']==0) & ((df['nu_pdg_final']== 14) | (df['nu_pdg_final']== -14))  & (df['nu_interaction_mode'] == 1)]#IsNC=0 means CC and 1 is NC
@@ -215,6 +229,7 @@ hknumuCCOther_enu_reco  = df.enu_reco[(df['IsNC']==0) & ((df['nu_pdg_final']== 1
 hknuEInclusive_enu_reco = df.enu_reco[(df['IsNC']==0) & ((df['nu_pdg_final']== 12) | (df['nu_pdg_final']== -12))]
 hkNCInclusive_enu_reco  = df.enu_reco[(df['IsNC']==1)]
 hext_enu_reco           = df_ext.enu_reco
+hdirt_enu_reco          = df_dirt.enu_reco
 
 hknumuCCQE_norm     = df.norm_weight[(df['IsNC']==0) & ((df['nu_pdg_final']== 14) | (df['nu_pdg_final']== -14))  & (df['nu_interaction_mode'] == 0)]#pdg == +-14 means muon and +-12 is electron
 hknumuRes_norm      = df.norm_weight[(df['IsNC']==0) & ((df['nu_pdg_final']== 14) | (df['nu_pdg_final']== -14))  & (df['nu_interaction_mode'] == 1)]#IsNC=0 means CC and 1 is NC
@@ -223,9 +238,10 @@ hknumuCCOther_norm  = df.norm_weight[(df['IsNC']==0) & ((df['nu_pdg_final']== 14
 hknuEInclusive_norm = df.norm_weight[(df['IsNC']==0) & ((df['nu_pdg_final']== 12) | (df['nu_pdg_final']== -12))]
 hkNCInclusive_norm  = df.norm_weight[(df['IsNC']==1)]
 hext_norm           = df_ext.norm_weight
+hdirt_norm          = df_dirt.norm_weight
 
-x = [hknumuCCQE_enu_reco, hknumuRes_enu_reco, hknumuMEC_enu_reco, hknumuCCOther_enu_reco, hknuEInclusive_enu_reco, hkNCInclusive_enu_reco, hext_enu_reco]
-y = [hknumuCCQE_norm, hknumuRes_norm, hknumuMEC_norm, hknumuCCOther_norm, hknuEInclusive_norm, hkNCInclusive_norm, hext_norm]
+x = [hknumuCCQE_enu_reco, hknumuRes_enu_reco, hknumuMEC_enu_reco, hknumuCCOther_enu_reco, hknuEInclusive_enu_reco, hkNCInclusive_enu_reco, hext_enu_reco, hdirt_enu_reco]
+y = [hknumuCCQE_norm, hknumuRes_norm, hknumuMEC_norm, hknumuCCOther_norm, hknuEInclusive_norm, hkNCInclusive_norm, hext_norm, hdirt_norm]
 
 #########################
 ##Reading the text file##
@@ -240,27 +256,33 @@ f = open('/uboone/data/users/shijy/Consistency/PeLEE_reco_nu_energy_digitized.cs
 for i in range(len(y1)):
     y1[i] = f.readline()
 
-total_y1 = 0
-for i in range(len(y1)):
-    total_y1 += y1[i]
+if Normalization is True:
+    total_y1 = 0
+    for i in range(len(y1)):
+        total_y1 += y1[i]
 
-y2 = y1
-for i in range(len(y1)):
-    y2[i] = y1[i]/total_y1
+    y2 = y1
+    for i in range(len(y1)):
+        y2[i] = y1[i]/total_y1
+else:
+    y2 = y1
 
 ############
 ##Plotting##
 ############
 
 plt.figure(figsize=(15,10))
-labels= [r"BNB $\nu_{\mu}$ CCQE", r"BNB $\nu_{\mu}$ Res",r"BNB $\nu_{\mu}$ MEC",r"BNB $\nu_{\mu}$ CCOther",r"$\nu_{e}$ Inclusive","NC Inlcusive", 'EXT','PeLEE tech note']
+#labels= [r"BNB $\nu_{\mu}$ CCQE", r"BNB $\nu_{\mu}$ Res",r"BNB $\nu_{\mu}$ MEC",r"BNB $\nu_{\mu}$ CCOther",r"$\nu_{e}$ Inclusive","NC Inlcusive", 'EXT', 'DIRT','PeLEE tech note']
+#labels= [r"BNB $\nu_{\mu}$ CCQE 0.368", r"BNB $\nu_{\mu}$ Res 0.262",r"BNB $\nu_{\mu}$ MEC 0.136",r"BNB $\nu_{\mu}$ CCOther 0.052",r"$\nu_{e}$ Inclusive 0.007","NC Inlcusive 0.060", 'EXT 0.085', 'DIRT 0.028','PeLEE tech note']#This is the label for normalized histogram
+#labels= [r"BNB $\nu_{\mu}$ CCQE 4655.96", r"BNB $\nu_{\mu}$ Res 3310.37",r"BNB $\nu_{\mu}$ MEC 1725.66",r"BNB $\nu_{\mu}$ CCOther 656.83",r"$\nu_{e}$ Inclusive 95.32","NC Inlcusive 762.95", 'EXT 1077.77', 'DIRT 353.85','PeLEE tech note 50925.99']#'f' is used to print variables
+labels= [r"BNB $\nu_{\mu}$ CCQE 13924.17", r"BNB $\nu_{\mu}$ Res 9900.03",r"BNB $\nu_{\mu}$ MEC 5160.79",r"BNB $\nu_{\mu}$ CCOther 1964.31",r"$\nu_{e}$ Inclusive 285.07","NC Inlcusive 2281.70", 'EXT 3223.19', 'DIRT 1058.23','PeLEE tech note 50925.99']#With POT scaling 6.37/2.13
 #plt.hist(x, bins=500, stacked=True,label=labels)
 #plt.hist(x, bins=14, range=(LOWER_LIMIT, UPPER_LIMIT), stacked=True,label=labels, weights=y)#Weights should have the same shape with data
 n, bins, patches = plt.hist(x, 14,histtype='bar',stacked=True, weights=y,
-                        color=['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink'],
-                        label=[r"BNB $\nu_{\mu}$ CCQE", r"BNB $\nu_{\mu}$ Res",r"BNB $\nu_{\mu}$ MEC",r"BNB $\nu_{\mu}$ CCOther",r"$\nu_{e}$ Inclusive","NC Inlcusive", 'EXT'])
+                        color=['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray'],
+                        label=[r"BNB $\nu_{\mu}$ CCQE", r"BNB $\nu_{\mu}$ Res",r"BNB $\nu_{\mu}$ MEC",r"BNB $\nu_{\mu}$ CCOther",r"$\nu_{e}$ Inclusive","NC Inlcusive", 'EXT', 'DIRT'])
 
-hatches = [' ',' ',' ',' ',' ',' ',' ']
+hatches = [' ',' ',' ',' ',' ',' ',' ',' ']
 for patch_set, hatch in zip(patches, hatches):
     for patch in patch_set.patches:
         patch.set_hatch(hatch)
@@ -278,15 +300,19 @@ hknumuMEC_patch      = mpatches.Patch(color='tab:green', label=r"BNB $\nu_{\mu}$
 hknumuCCOther_patch  = mpatches.Patch(color='tab:red', label=r"BNB $\nu_{\mu}$ CC Other")
 hknuEInclusive_patch = mpatches.Patch(color='tab:purple', label=r"$\nu_{e}$ Inclusive")
 hkNCInclusive_patch  = mpatches.Patch(color='tab:brown', label="NC Inlcusive")
-hext_patch  = mpatches.Patch(color='tab:pink', label="EXT")
+hext_patch           = mpatches.Patch(color='tab:pink', label="EXT")
+hdirt_patch          = mpatches.Patch(color='tab:gray', label="DIRT")
 #hext_patch           = mpatches.Patch(fill=False, hatch='\\')#If you want the tech note hatch
 x1_line              = Line2D([0], [0], color='k', linewidth=2, label="PeLEE tech note")
 
-handle_me = [hknumuCCQE_patch,hknumuRes_patch,hknumuMEC_patch,hknumuCCOther_patch,hknuEInclusive_patch,hkNCInclusive_patch,hext_patch,x1_line]
+handle_me = [hknumuCCQE_patch,hknumuRes_patch,hknumuMEC_patch,hknumuCCOther_patch,hknuEInclusive_patch,hkNCInclusive_patch,hext_patch,hdirt_patch,x1_line]
 plt.legend(handle_me, labels,fontsize=20)
 #plt.yscale('log')
 #plt.legend()
-plt.suptitle('PeLEE Neutrino reconstructed energy (GENIE, POT weight included, normalized)',fontsize=22)
+if Normalization is True:
+    plt.suptitle('PeLEE Neutrino reconstructed energy (GENIE, POT weight included, normalized)',fontsize=22)
+else:
+    plt.suptitle('PeLEE Neutrino reconstructed energy (GENIE, POT weight included)',fontsize=22)
 plt.rc('xtick',labelsize=22)
 plt.rc('ytick',labelsize=22)
 params = {'axes.labelsize': 22,'axes.titlesize':22, 'legend.fontsize': 20, 'xtick.labelsize': 22, 'ytick.labelsize': 22}#text.fontsize
